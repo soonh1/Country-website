31b28b9fe07551cefedd0603071befd8d13dd5e6
Jakobbnielsen <32448314+Jakobbnielsen@users.noreply.github.com>
2021-03-01 12:48:25 +0100

    asd

--- countries-website/src/components/CountryInfo.vue
+++ countries-website/src/components/CountryInfo.vue
@@ -2,7 +2,14 @@
 <h2>Country Info Component</h2>
 <div v-if="item">
 <div>Name: {{ name }}</div>
-<div>Population: {{ item.population }}</div>
+<div>Population: {{ item.[0].population }}</div>
+<div>Region: {{ item.[0].region }}</div>
+<div>Capital: {{ item.[0].capital }}</div>
+<div>Native Name: {{ item.[0].nativeName }}</div>
+<div>Subregion: {{ item.[0].subregion }}</div>
+<div>Top level domain: {{ item.[0].topLevelDomain.[0] }}</div>
+<div>Languages: {{ item.[0].languages.[0].name }}</div>
+<div>Currencies: {{ item.[0].currencies.[0].name }}</div>
 </div>
 </template>
 
@@ -18,7 +25,7 @@ props: ["name"],
 created() {
 axios({
   method: "get",
-  url: "https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;nativeName;subregion;topLevelDomain;currencies;languages;borders" + this.name,
+  url: "https://restcountries.eu/rest/v2/name/" + this.name,
 })
   .then((response) => {
     this.item = response.data;
