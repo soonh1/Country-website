2f784422682156f282ae74ba462959409ff2f454
soonh1 <johnny_tran_3@hotmail.com>
2021-03-01 09:51:53 +0100

    Header update

--- countries-website/src/components/Header.vue
+++ countries-website/src/components/Header.vue
@@ -1,20 +1,24 @@
 <template>
-  <div class="header">
-    <h1>This is a header</h1>
-  </div>
+  <header>
+    <h1>Where in the world?</h1>
+    <button>Dark mode</button>
+  </header>
 </template>
 <script>
 export default {};
 </script>
 <style scoped lang="scss">
-$base-color: #c6538c;
-
-.header {
-  background-color: $base-color;
+header {
+  box-shadow: 1px 1px 10px #888888;
   height: 100px;
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-gap: 500px;
   h1 {
-    margin: 0;
-    padding: 0;
+    margin: auto 50px;
+    text-align: start;
+  }
+  button {
   }
 }
 </style>
